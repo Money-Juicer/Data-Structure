28f057dfcaa7033c077ad9d48e7cde67af278776
timel2ss <dyddn0823@hanyang.ac.kr>
2022-01-23 17:20:17 +0900

    Structure 정의

---
+++ src/main/java/List/ArrayList.java
@@ -0,0 +1,37 @@
+package List;
+
+/**
+ *
+ * @param <E>
+ */
+public class ArrayList<E> implements List<E> {
+    @Override
+    public void insert(Node<E> node) {
+
+    }
+
+    @Override
+    public void insert(int index, Node<E> node) {
+
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+
+    @Override
+    public Node<E> find(E element) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void delete(Node<E> node) {
+
+    }
+}
---
+++ src/main/java/List/LinkedList.java
@@ -0,0 +1,38 @@
+package List;
+
+/**
+ * Jihoon
+ * @param <E> element
+ */
+public class LinkedList<E> implements List<E> {
+
+    @Override
+    public void insert(Node<E> node) {
+
+    }
+
+    @Override
+    public void insert(int index, Node<E> node) {
+
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+
+    @Override
+    public Node<E> find(E element) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void delete(Node<E> node) {
+
+    }
+}
