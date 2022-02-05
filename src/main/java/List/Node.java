4d742b754f3875d739ccb17119bfd02eb47ed551
timel2ss <dyddn0823@hanyang.ac.kr>
2022-02-06 01:18:46 +0900

    feat: Node, Student 생성자 추가

--- src/main/java/List/Node.java
+++ src/main/java/List/Node.java
@@ -4,4 +4,8 @@ public class Node<E> {
     private E element;
     private Node<E> next;
     private Node<E> prev;
+
+    public Node(E element) {
+        this.element = element;
+    }
 }
--- src/main/java/student/Student.java
+++ src/main/java/student/Student.java
@@ -1,7 +1,32 @@
 package student;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Student {
     private String name;
     private Long id;
     private Sex sex;
+
+    public Student(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "]" + " " + name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof Student)) {
+            return false;
+        }
+
+        return id.equals(((Student) obj).id);
+    }
 }
