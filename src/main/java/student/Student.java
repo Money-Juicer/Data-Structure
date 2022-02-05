94a2d0bcc917a8d92302bd9f36a59de1edbac459
2019089452 <ryuhaksrg@hanyang.ac.kr>
2022-02-05 16:20:07 +0900

    LinkedList

--- src/main/java/student/Student.java
+++ src/main/java/student/Student.java
@@ -1,7 +1,16 @@
 package student;
 
 public class Student {
+    public Student(String name, long id, Sex sex){
+        this.name = name;
+        this.id = id;
+        this.sex = sex;
+    }
+    public void print()
+    {
+        System.out.println("Name = "+name +", id = "+id+", Sex = "+sex);
+    }
     private String name;
-    private Long id;
+    private long id;
     private Sex sex;
 }
